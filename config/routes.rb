53a1b9618f346f98a10b4eb9ae69910e6e8db751
Tom Scott <tom.a.scott@hotmail.co.uk>
2016-06-04 13:33:02 +0100

    added comment scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  
 
+  resources :comments
   devise_for :users
   root 'statics#homepage'
   get 'users', to: 'users#index'
---
+++ db/migrate/20160604123228_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :name
+      t.text :body
+      t.integer :event_id
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
